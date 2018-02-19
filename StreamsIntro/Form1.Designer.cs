0ad1b2a210cee406cdb547d0ba4f926559e8ce56
89382 <paul.laird@dbs.ie>
2018-02-19 14:39:50 +0000

    initial

---
+++ StreamsIntro/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace StreamsIntro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbFileContents = new System.Windows.Forms.RichTextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.txtStreamProperties = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbFileContents
+            // 
+            this.rtbFileContents.Location = new System.Drawing.Point(0, 3);
+            this.rtbFileContents.Name = "rtbFileContents";
+            this.rtbFileContents.ReadOnly = true;
+            this.rtbFileContents.Size = new System.Drawing.Size(284, 152);
+            this.rtbFileContents.TabIndex = 0;
+            this.rtbFileContents.Text = "";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(30, 190);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Enabled = false;
+            this.btnReadFile.Location = new System.Drawing.Point(175, 190);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // txtStreamProperties
+            // 
+            this.txtStreamProperties.Location = new System.Drawing.Point(0, 234);
+            this.txtStreamProperties.Multiline = true;
+            this.txtStreamProperties.Name = "txtStreamProperties";
+            this.txtStreamProperties.ReadOnly = true;
+            this.txtStreamProperties.Size = new System.Drawing.Size(284, 83);
+            this.txtStreamProperties.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 318);
+            this.Controls.Add(this.txtStreamProperties);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.rtbFileContents);
+            this.Name = "Form1";
+            this.Text = "Streams Intro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbFileContents;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.TextBox txtStreamProperties;
+    }
+}
+
---
+++ StreamsIntro/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamsIntro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            // Lets create a destination backing store stream
+            using (Stream fileStream = new FileStream(@"C:\AdvProg\AdvProg.txt", 
+                                        FileMode.OpenOrCreate))
+            {
+                /*
+                 * Note, that if the file we are opening is read-only, we must use
+                 * a different constructor--typically one of the 3 or 4 parameter
+                 * ones that take a FileAccess enum which should be set to
+                 * FileAccess.Read
+                 */
+
+                btnOpenFile.Enabled = false;
+
+                DisplayStreamProperties(fileStream);
+
+                // Write just a byte to the stream
+                fileStream.WriteByte(42);
+
+                // And now write out byte array
+                byte[] primeVals = { 2, 3, 5, 7, 11 };
+                fileStream.Write(primeVals, 0, primeVals.Length);
+
+                string msg = $"The stream's current position is now {fileStream.Position}";
+
+                MessageBox.Show(msg, "Stream Position Property");
+
+                /*
+                 * Note:  we do not explicitly call Flush() or Close() on the stream. Instead
+                 * we rely on the using statement which will implicitly call these on our
+                 * behalf.
+                 */
+
+                btnReadFile.Enabled = true;
+            }
+        }
+
+        private void DisplayStreamProperties(Stream fileStream)
+        {
+            txtStreamProperties.Text = $"Stream CanSeek value is {fileStream.CanSeek}"
+                                        + Environment.NewLine +
+                                       $"Stream CanWrite value is {fileStream.CanWrite}"
+                                        + Environment.NewLine +
+                                       $"Stream CanRead value is {fileStream.CanRead}"
+                                        + Environment.NewLine +
+                                       $"Stream CanTimeout is {fileStream.CanTimeout}"
+                                        + Environment.NewLine +
+                                       $"Stream's current position is {fileStream.Position}";
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            // Here we create a source backing store
+            using (Stream fileStream = new FileStream(@"C:\AdvProg\AdvProg.txt", FileMode.Open))
+            {
+                // First we read a simple byte from the stream...
+                int valueRead = fileStream.ReadByte();
+                rtbFileContents.Text = valueRead + Environment.NewLine;
+
+                // ...and now we read the rest of the stream contents into a byte array
+                byte[] primeVals = new byte[fileStream.Length - fileStream.Position];
+                fileStream.Read(primeVals, 0, primeVals.Length);
+                foreach (var prime in primeVals)
+                {
+                    rtbFileContents.AppendText(prime + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
